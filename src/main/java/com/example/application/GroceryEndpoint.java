f32e8c47fa5040e7a4054b1106a6c9992f3973d8
osvaldsoza <osvald.soza@gmail.com>
2022-03-28 23:14:17 -0300

    first commit

--- src/main/java/com/example/application/GroceryEndpoint.java
+++ src/main/java/com/example/application/GroceryEndpoint.java
@@ -1,2 +1,24 @@
-package com.example.application;public class GroceryEndpoint {
+package com.example.application;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.Endpoint;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Endpoint
+@AnonymousAllowed
+public class GroceryEndpoint {
+
+    private final List<GroceryItem> groceryList = new ArrayList<>();
+
+    public @NotNull List<@NotNull GroceryItem> getGroceries() {
+        return groceryList;
+    }
+
+    public GroceryItem save(GroceryItem groceryItem) {
+        groceryList.add(groceryItem);
+        return groceryItem;
+    }
 }
--- src/main/java/com/example/application/GroceryItem.java
+++ src/main/java/com/example/application/GroceryItem.java
@@ -1,2 +1,31 @@
-package com.example.application;public class GroceryItem {
+package com.example.application;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class GroceryItem {
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Min(value = 1)
+    private Integer quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
